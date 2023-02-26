b1c14153e76d7bc3c29f25c99be63ebbff0e2493
88plug <19512127+88plug@users.noreply.github.com>
2023-02-26 11:19:44 -0800

    Update Miner.h

--- src/core/Miner.h
+++ src/core/Miner.h
@@ -19,9 +19,9 @@
 #ifndef XMRIG_MINER_H
 #define XMRIG_MINER_H
 
-#include <memory>
-#include <unordered_map>
+
 #include <vector>
+
 
 #include "backend/common/interfaces/IRxListener.h"
 #include "base/api/interfaces/IApiListener.h"
@@ -30,50 +30,53 @@
 #include "base/kernel/interfaces/ITimerListener.h"
 #include "base/tools/Object.h"
 
+
 namespace xmrig {
+
 
 class Controller;
 class Job;
 class MinerPrivate;
 class IBackend;
 
-class Miner : public ITimerListener, public IBaseListener, public IApiListener, public IRxListener {
+
+class Miner : public ITimerListener, public IBaseListener, public IApiListener, public IRxListener
+{
 public:
     XMRIG_DISABLE_COPY_MOVE_DEFAULT(Miner)
 
-    Miner(Controller* controller);
+    Miner(Controller *controller);
     ~Miner() override;
 
     bool isEnabled() const;
-    bool isEnabled(const Algorithm& algorithm) const;
-    const Algorithms& algorithms() const;
-    const std::vector<IBackend*>& backends() const;
-    const Job& job() const;
+    bool isEnabled(const Algorithm &algorithm) const;
+    const Algorithms &algorithms() const;
+    const std::vector<IBackend *> &backends() const;
+    Job job() const;
     void execCommand(char command);
     void pause();
     void setEnabled(bool enabled);
-    void setJob(const Job& job, bool donate);
+    void setJob(const Job &job, bool donate);
     void stop();
 
 protected:
-    void onConfigChanged(Config* config, Config* previousConfig) override;
-    void onTimer(const Timer* timer) override;
+    void onConfigChanged(Config *config, Config *previousConfig) override;
+    void onTimer(const Timer *timer) override;
 
-#ifdef XMRIG_FEATURE_API
-    void onRequest(IApiRequest& request) override;
-#endif
+#   ifdef XMRIG_FEATURE_API
+    void onRequest(IApiRequest &request) override;
+#   endif
 
-#ifdef XMRIG_ALGO_RANDOMX
+#   ifdef XMRIG_ALGO_RANDOMX
     void onDatasetReady() override;
-#endif
+#   endif
 
 private:
-    std::unique_ptr<MinerPrivate> m_private;
-    std::vector<bool> m_algorithmEnabled;
-    std::unordered_map<const Algorithm*, size_t> m_algorithmIndexMap;
+    MinerPrivate *d_ptr;
 };
 
-}  // namespace xmrig
 
-#endif  // XMRIG_MINER_H
+} // namespace xmrig
 
+
+#endif /* XMRIG_MINER_H */
